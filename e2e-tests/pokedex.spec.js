085bd0646d0a4f1cf43a422be8c665672a242a47
Diogo Silva <73388219+clebinho1234@users.noreply.github.com>
2024-06-21 10:57:36 +0100

    Update pokedex.spec.js

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,7 +6,7 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
-  
+
   test('pokemon page can be navigated to', async ({ page }) => {
     await page.goto('http://localhost:8080')
     await page.click('a[href="/pokemon/ivysaur"]')
